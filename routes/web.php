0e272ac203f5de6edca8304c5395067b13d196fc
Jetulio <joo_ramos2006@hotmail.com>
2026-03-11 11:16:49 -0300

    feat: added a register route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
 
 Route::get('/', [ChirpController::class, 'index']);
 // Route::get('/chirps/{chirp}', [ChirpController::class, 'show']);
@@ -9,5 +10,5 @@ Route::get('/', [ChirpController::class, 'index']);
 // Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 // Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 // Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
-// Route::get()
+Route::resource('auth', Register::class)->only(['__invoke']);
 Route::resource('chirps', ChirpController::class)->only(['store', 'edit', 'update', 'destroy']);
