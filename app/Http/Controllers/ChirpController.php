77cbc81b78e3ed4682cd23cdbd5d2a8096e6177a
Jetulio <joo_ramos2006@hotmail.com>
2026-03-13 15:36:29 -0300

    fix: silenced unused resource

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -22,10 +22,10 @@ class ChirpController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
